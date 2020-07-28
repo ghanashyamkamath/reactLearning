57ac23ef304727a9b55bd1db647643177186bc3e
Ghanashyam Kamath <ghanashyam.kamath99@gmail.com>
2020-07-28 15:38:18 +0530

    npm ejecting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';  
 // import React, { useState } from 'react';
-import Radium , { StyleRoot } from 'radium';
+// import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -77,10 +77,10 @@ class App extends Component {
       border:'1px solid blue',
       padding:'8px',
       cursor:'pointer',
-      ':hover':{
-        backgroundColor:'lightgreen',
-        color:'black'
-      }
+      // ':hover':{
+      //   backgroundColor:'lightgreen',
+      //   color:'black'
+      // }
 
     };
 
@@ -121,10 +121,10 @@ class App extends Component {
 
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor:'lightred',
-        color:'black'
-      }
+      // style[':hover'] = {
+      //   backgroundColor:'lightred',
+      //   color:'black'
+      // }
 
     }
     let classes=[];
@@ -137,7 +137,7 @@ class App extends Component {
 
 
     return (
-      <StyleRoot>
+      
       <div className="App">
         <h1>Welcome Ghanashyam</h1>
         <p className = {classes.join(' ')}>This is working!!!!!!!!!</p>
@@ -150,9 +150,9 @@ class App extends Component {
         
         
       </div>
-      </StyleRoot>
+      
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Radium from 'radium';
+
 import './Person.css';
 
 
@@ -8,16 +8,16 @@ import './Person.css';
 const person = (props) =>{
 
     //media queries
-    const style ={
-        '@media (min-width : 500px)': {
-            width :'450px'
-        }
-    };
+    // const style ={
+    //     '@media (min-width : 500px)': {
+    //         width :'450px'
+    //     }
+    // };
     
 
 
     return(
-        <div className="Person" style={style}>
+        <div className="Person"> 
         <p onClick={props.click}>I am {props.name} and my age is {props.age} years old.</p> 
          {/* dynamic content  */}
 
@@ -28,4 +28,4 @@ const person = (props) =>{
         </div>
     );
 };
-export default Radium(person); 
+export default person; 
